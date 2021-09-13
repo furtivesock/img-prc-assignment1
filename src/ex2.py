af49c0e39cf505bc9d093c6e1101265487c96295
NGUYEN, Sophie <chaussettefurtive@outlook.fr>
2021-09-13 12:43:15 +0200

    Read user margin of error

--- src/ex2.py
+++ src/ex2.py
@@ -4,17 +4,45 @@
 Assess precision of a suggested solution of fragments coordinates and orientation based on the real solution
 Run by providing the following parameters:
 
-ex2 [Dx (px)] [Dy (px)] [Dalpha (deg)]
+./ex2.py [Dx (px)] [Dy (px)] [Dalpha (deg)]
 
 Authors: Sophie Nguyen <sophie.nguyen@universite-paris-saclay.fr>, Tom Mansion <tom.mansion@universite-paris-saclay.fr>
 """
+
+import sys
+
+# Default margins to make the coding easier
+DX = 3
+DY = 3
+DALPHA = 5
+
+class Margin:
+    """Fragment margin of error set by user
+
+    Attributes:
+        dx          Horizontal positioning margin
+        dy          Vertical positioning margin
+        dalpha      Orientation margin
+    """
+    def __init__(self, dx, dy, dalpha):
+        self.dx = dx
+        self.dy = dy
+        self.dalpha = dalpha
 
 # TODO: Fragment comparison function: returns true if the difference in x, y and orientation alpha is lower than the set parameters, returns false is not
 # TODO: Compute fragment surface function
 
 # TODO: MAIN FUNCTION 
 
-# TODO: Retrieve user parameters and store it in ref object
+if __name__ == '__main__':
+    margin = None
+
+    if len(sys.argv) != 4:
+        margin = Margin(DX, DY, DALPHA)
+    else:
+        margin = Margin(sys.argv[1], sys.argv[2], sys.argv[3])
+
+
 # TODO: Read suggested solution.txt file, store the fragments data in list
 # TODO: Read real solution fragments.txt file, store the solution fragments data in list
 
